c622d4f979191ef1cd937ac1c891d5009015921a
orun-sunny <sunny.mukut@gmail.com>
2026-07-26 23:43:16 +0600

    new migration added

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,6 +6,7 @@ namespace BookCrud.Models
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
-        public DateTime Created { get; set; }
+        public DateTime Created { get; set; };
+        public double Price { get; set; } = 0.0;
     }
 }
--- obj/Debug/net10.0/..AssemblyInfo.cs
+++ obj/Debug/net10.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0756aa58402bb554b6887e2bb65aaabe95bedb")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
